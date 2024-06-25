36825f668571d4008495c7a76b5c07a17a4dd314
Halley Pham <xhalleyy@gmail.com>
2024-06-25 10:00:00 -0700

    datepicker fix and responsiveness

--- src/app/components/FormComponent.tsx
+++ src/app/components/FormComponent.tsx
@@ -1,9 +1,15 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import { Datepicker, TextInput } from "flowbite-react";
+import { CustomFlowbiteTheme, Datepicker, TextInput } from "flowbite-react";
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-import { Tooltip } from '@mui/material';
+import { TextField, Tooltip } from '@mui/material';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs, { Dayjs } from 'dayjs';
+import { error } from 'console';
 
 type FormProp = {
     success: boolean | undefined
@@ -11,7 +17,7 @@ type FormProp = {
 }
 
 const FormComponent = ({ success, setSuccess }: FormProp) => {
-    const [form, setForm] = useState({ firstName: '', lastName: '', email: '', birthday: '', address: '', phoneNumber: '', password: '', confirmPassword: '' })
+    const [form, setForm] = useState({ firstName: '', lastName: '', email: '', birthday: null as Dayjs | null, address: '', phoneNumber: '', password: '', confirmPassword: '' })
     const [emailError, setEmailError] = useState('');
     const [phoneError, setPhoneError] = useState('');
     const [isSubmitted, setIsSubmitted] = useState(false)
@@ -19,7 +25,11 @@ const FormComponent = ({ success, setSuccess }: FormProp) => {
     const [passVisibility, setPassVisibility] = useState(false)
     const [confirmVisibility, setConfirmVisibility] = useState(false)
 
-    const isFilled = form.firstName !== "" && form.lastName !== "" && form.email !== "" && form.password !== "" && form.confirmPassword !== "";
+    const [errors, setErrors] = useState({
+        birthday: false
+    });
+
+    const isFilled = form.firstName !== "" && form.lastName !== "" && form.email !== "" && form.password !== "" && form.confirmPassword !== "" && form.birthday !== null;
     // const isFilled = Object.values(form).every(value => value !== "");
 
     const validEmail = (email: string) => {
@@ -40,9 +50,15 @@ const FormComponent = ({ success, setSuccess }: FormProp) => {
         return true;
     };
 
-    const handleDateChange = (date: React.ChangeEvent<HTMLInputElement>) => {
-        const selectedDate = date.target.value; // Extract the date value from the event
-        setForm({ ...form, birthday: selectedDate });
+    const handleDateChange = (date: Dayjs | null) => {
+        setForm({
+            ...form,
+            birthday: date
+        });
+        setErrors({
+            ...errors,
+            birthday: date === null
+        });
     };
 
     const updateForm = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -68,13 +84,18 @@ const FormComponent = ({ success, setSuccess }: FormProp) => {
         if (form.phoneNumber !== '') {
             isPhoneValid = validPhoneNumber(form.phoneNumber)
         }
-        if (isFilled && isEmailValid && isPhoneValid && matchedPasswords) {
+
+        if (!form.birthday) {
+            setErrors({ ...errors, birthday: true });
+        }
+
+        if (isFilled && isEmailValid && isPhoneValid && matchedPasswords && !errors.birthday) {
             setSuccess(true);
         }
 
 
         setEmailError(isEmailValid ? '' : 'Invalid email format. Please make sure there is an @ with no spaces.');
-        setPhoneError(isPhoneValid ? '' : 'Invalid phone number format, ex: (XXX)-XXX-XXXX');
+        setPhoneError(isPhoneValid ? '' : 'Invalid phone number format, ex: (XXX)-###-####');
 
         setIsSubmitted(true);
     }
@@ -85,7 +106,7 @@ const FormComponent = ({ success, setSuccess }: FormProp) => {
                 firstName: '',
                 lastName: '',
                 email: '',
-                birthday: '',
+                birthday: null,
                 address: '',
                 phoneNumber: '',
                 password: '',
@@ -101,78 +122,128 @@ const FormComponent = ({ success, setSuccess }: FormProp) => {
         }
     }, [success]);
 
+    // useEffect(()=>{console.log(form)},[form])
+
+    const customText: CustomFlowbiteTheme['textInput'] = {
+        "base": "flex",
+        "addon": "inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400",
+        "field": {
+            "base": "relative w-full",
+            "icon": {
+                "base": "pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3",
+                "svg": "h-5 w-5 text-gray-500 dark:text-gray-400"
+            },
+            "rightIcon": {
+                "base": "pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3",
+                "svg": "h-5 w-5 text-gray-500 dark:text-gray-400"
+            },
+            "input": {
+                "base": "block w-full border disabled:cursor-not-allowed disabled:opacity-50",
+                "sizes": {
+                    "sm": "p-2 sm:text-xs",
+                    "md": "p-2 text-sm",
+                    "lg": "p-4 sm:text-base"
+                },
+                "colors": {
+                    "white": "border-gray-400 bg-white focus:border-blue-500 focus:ring-blue-500",
+                },
+                "withRightIcon": {
+                    "on": "pr-10",
+                    "off": ""
+                },
+                "withIcon": {
+                    "on": "pl-10",
+                    "off": ""
+                },
+                "withAddon": {
+                    "on": "rounded-r-lg",
+                    "off": "rounded"
+                },
+                "withShadow": {
+                    "on": "shadow-sm dark:shadow-sm-light",
+                    "off": ""
+                }
+            }
+        }
+    }
+
     return (
-        <div className='bg-white/90 border-4 border-[#fe81bb] h-[630px] w-[550px] rounded-3xl shadow-md'>
+        <div className='bg-white/90 border-4 border-[#fe81bb] h-[670px] md:h-[630px] md:w-[550px] rounded-3xl shadow-md'>
 
             <form onSubmit={handleForm} action="" className='pt-10'>
                 <div className='grid grid-cols-3 items-center mb-4'>
-                    <label className='ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
-                        First Name*
+                    <label className='ps-2 md:ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
+                        First Name *
                     </label>
-                    <TextInput value={form.firstName} onChange={updateForm} type="text" className="col-span-2 me-10 bg-white active:bg-[#feffc7] focus-within:bg-[#feffc7]" placeholder="Enter First Name" name="firstName" required maxLength={100} />
+                    <TextInput theme={customText} color="white" value={form.firstName} onChange={updateForm} type="text" className="col-span-2 mx-3 md:me-10 bg-white active:bg-[#feffc7] focus-within:bg-[#feffc7]" placeholder="Enter First Name" name="firstName" required maxLength={100} />
                 </div>
                 <div className='grid grid-cols-3 items-center mb-4'>
-                    <label className='ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
-                        Last Name*
+                    <label className='ps-2 md:ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
+                        Last Name *
                     </label>
-                    <TextInput value={form.lastName} onChange={updateForm} type="text" className="col-span-2 me-10" placeholder="Enter Last Name" name="lastName" required maxLength={100} />
-                    {/* <input value={form.lastName} onChange={updateForm} type="text" className="col-span-2 border rounded p-2 me-10" placeholder="Enter Last Name" name="lastName" required maxLength={100} /> */}
+                    <TextInput theme={customText} color="white" value={form.lastName} onChange={updateForm} type="text" className="col-span-2 mx-3 md:me-10" placeholder="Enter Last Name" name="lastName" required maxLength={100} />
                 </div>
                 <div className='grid grid-cols-3 items-center mb-4'>
-                    {isSubmitted && emailError && <div className="text-red-500 col-span-3 flex justify-center text-sm font-bold">{emailError}</div>}
-                    <label className='ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
-                        Email*
+                    {isSubmitted && emailError && <div className="text-red-500 col-span-3 text-center px-3 flex justify-center text-sm font-bold">{emailError}</div>}
+                    <label className='ps-2 md:ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
+                        Email *
                     </label>
-                    <TextInput value={form.email} onChange={updateForm} type="text" className="col-span-2 me-10" placeholder="ex: hello123@gmail.com" name="email" required />
-                    {/* <input value={form.email} onChange={updateForm} type="text" className="col-span-2 border rounded p-2 me-10" placeholder="ex: hello123@gmail.com" name="email" required /> */}
+                    <TextInput theme={customText} color="white" value={form.email} onChange={updateForm} type="text" className="col-span-2 mx-3 md:me-10" placeholder="ex: hello123@gmail.com" name="email" required />
                 </div>
                 <div className='grid grid-cols-3 items-center mb-4'>
-                    <label className='ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
-                        Birthday*
+                    {isSubmitted && errors.birthday && <div className="text-red-500 col-span-3 text-center px-3 flex justify-center text-sm font-bold">Please enter your birthday</div>}
+                    <label className='ps-2 md:ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
+                        Birthday *
                     </label>
-                    <Datepicker
-                        // value={form.birthday}
-                        onChange={handleDateChange}
-                        className=""
-                        maxDate={new Date()}
-                        name="birthday"
-                        id="birthday"
-                        required
-                    />
+                    <div className='col-span-2 mx-3 md:me-10 flex items-center'>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker
+                                label="mm/dd/yyyy"
+                                value={form.birthday}
+                                disableFuture
+                                onChange={handleDateChange}
+                                // renderInput={(params: any) => <TextField {...params} />}
+                                slotProps={{
+                                    textField: {
+                                        required: true,
+                                        error: errors.birthday,
+                                        title: "Please enter a birthday"
+                                    }
+                                }}
+                            />
+                        </LocalizationProvider>
+
+                    </div>
                 </div>
                 <div className='grid grid-cols-3 items-center mb-4'>
-                    <label className='ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
+                    <label className='ps-2 md:ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
                         Home Address
                     </label>
-                    <TextInput value={form.address} onChange={updateForm} type="text" className="col-span-2 me-10" placeholder="Enter Address" name="address" maxLength={100} />
-                    {/* <input value={form.address} onChange={updateForm} type="text" className="col-span-2 border rounded p-2 me-10" placeholder="Enter Address" name="address" maxLength={100} /> */}
+                    <TextInput theme={customText} color="white" value={form.address} onChange={updateForm} type="text" className="col-span-2 mx-3 md:me-10" placeholder="Enter Address" name="address" maxLength={100} />
                 </div>
                 <div className='grid grid-cols-3 items-center mb-4'>
-                    {isSubmitted && phoneError && <div className="text-red-500 col-span-3 flex justify-center text-sm font-bold">{phoneError}</div>}
-                    <label className='ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
+                    {isSubmitted && phoneError && <div className="text-red-500 col-span-3 text-center px-3 flex justify-center text-sm font-bold">{phoneError}</div>}
+                    <label className='ps-2 md:ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
                         Phone Number
                     </label>
-                    <TextInput value={form.phoneNumber} onChange={updateForm} type="text" className="col-span-2 me-10" placeholder="ex: (XXX)-XXX-XXXX" name="phoneNumber" />
-                    {/* <input value={form.phoneNumber} onChange={updateForm} type="text" className="col-span-2 border rounded p-2 me-10" placeholder="ex: (XXX)-XXX-XXXX" name="phoneNumber" /> */}
+                    <TextInput theme={customText} color="white" value={form.phoneNumber} onChange={updateForm} type="text" className="col-span-2 mx-3 md:me-10" placeholder="ex: (XXX)-###-####" name="phoneNumber" />
                 </div>
                 <div className='grid grid-cols-3 items-center mb-4'>
-                    <label className='ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
-                        Password*
+                    <label className='ps-2 md:ps-4 col-span-1 text-center font-darling text-lg text-[#752727] flex justify-center' htmlFor="block rounded p-3">
+                        Password *
                     </label>
-                    <TextInput value={form.password} onChange={updateForm} type="password" className="col-span-2 me-10" placeholder="Enter Password" name="password" required
+                    <TextInput theme={customText} color="white" value={form.password} onChange={updateForm} type="password" className="col-span-2 mx-3 md:me-10" placeholder="Enter Password" name="password" required
                         minLength={15}
                         pattern="^(?=.*[A-Z])(?=.*[0-9])(?=.*[?@#$%^&*])[A-Za-z0-9?@#$%^&*]{15,}$"
                         title="Password must be at least 15 characters and have at least one capital letter, one number, and one special character (? @ # $ % ^ & *)."
-                        />
+                    />
                 </div>
                 <div className='grid grid-cols-3 items-center mb-4'>
-                    {isSubmitted && passwordError && <div className="text-red-500 col-span-3 text-sm font-bold flex justify-center">{passwordError}</div>}
-                    <label className='ps-4 col-span-1 text-center flex justify-center font-darling text-lg text-[#752727]' htmlFor="block rounded p-3">
-                        Confirm Password*
+                    {isSubmitted && passwordError && <div className="text-red-500 col-span-3 text-center px-3 flex justify-center text-sm font-bold">{passwordError}</div>}
+                    <label className='ps-2 md:ps-4 col-span-1 text-center flex justify-center font-darling text-lg text-[#752727]' htmlFor="block rounded p-3">
+                        Confirm Password *
                     </label>
-                    <TextInput value={form.confirmPassword} onChange={updateForm} type="password" className="col-span-2 me-10 " placeholder="Confirm Password" name="confirmPassword" required minLength={15} />
-                    {/* <input value={form.confirmPassword} onChange={updateForm} type="password" className="col-span-2 border rounded p-2 me-10" placeholder="Confirm Password" name="confirmPassword" required minLength={15}
-                    /> */}
+                    <TextInput theme={customText} color="white" value={form.confirmPassword} onChange={updateForm} type="password" className="col-span-2 mx-3 md:me-10 " placeholder="Confirm Password" name="confirmPassword" required minLength={15} />
                 </div>
 
                 <div className='absolute bottom-6 right-7'>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,16 @@ export default function Home() {
   const [success, setSuccess] = useState<boolean | undefined>(undefined);
   return (
     <div className="backgroundImg relative">
-      <div className='absolute top-10 right-10'>
+      <div className='absolute top-3 md:top-8 lg:top-10 right-2 md:right-10'>
         {success && (
           <div className="w-72 rounded-xl">
-            <Alert className='rounded-xl bg-paleblue flex items-center' icon={<Image src={'/success.png'} width={30} height={30} alt='Successfully submitted!' />} severity="success">
-              Successfully submitted! <span className="underline hover:italic ">View your submission!</span>
+            <Alert className='rounded-xl bg-paleblue flex items-center' icon={<Image src={'/success.png'} width={35} height={35} alt='Successfully submitted!' />} severity="success">
+              Successfully submitted!
             </Alert>
           </div>
         )}
-        {/* {success == false && (
-          <div className="w-72 rounded-xl">
-            <Alert className='rounded-xl bg-rose-200 flex items-center font-mainFont text-lg' icon={<Image src={'/fail.png'} width={30} height={30} alt='Failed to update' />} severity="error">
-              Fields were empty!
-            </Alert>
-          </div>
-        )} */}
       </div>
-      <div className="absolute top-12 left-28">
+      <div className="absolute top-28 md:top-32 lg:top-12 md:left-28">
         <div className="relative">
           <h1 className="absolute font-darling text-5xl -top-6 left-[33%] text-[#752727] text-shadow">Sign Up</h1>
           <FormComponent success={success} setSuccess={setSuccess} />
